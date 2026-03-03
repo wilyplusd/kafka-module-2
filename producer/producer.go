9c9e4212cff0cc56809c19e59da13cbdb0ca9488
Ira Derevianko <wilyplus@gmail.com>
2026-03-03 22:03:26 +0300

    Fix producer

--- producer/producer.go
+++ producer/producer.go
@@ -1,51 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
-    "context"
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-    "github.com/segmentio/kafka-go"
-    "kafka-module-2/models"
+	"kafka-module-2/models"
+
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-    // Создаём kafka-продюсера
-    writer := kafka.NewWriter(kafka.WriterConfig{
-        Brokers:  []string{"localhost:9092"},
-        Topic:    "my-topic",
-        Balancer: &kafka.LeastBytes{},
-        Async:    false,
-    })
+	// Создаём kafka-продюсера
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers:      []string{"localhost:9092"},
+		Topic:        "my-topic",
+		Balancer:     &kafka.LeastBytes{},
+		RequiredAcks: -1,
+		MaxAttempts:  10,
+		Async:        false,
+	})
 
-    // Закрываем reader при выходе из main
-    defer writer.Close()
+	// Закрываем writer при выходе из main
+	defer writer.Close()
 
-    // Создаём контекст
-    ctx := context.Background()
+	// Создаём контекст
+	ctx := context.Background()
 
-    // Основной бесконечный цикл записи сообщений
-    for i := 1; ; i++ {
-        // Создаём сообщение с уникальным ID и текстом
-        msg := &models.Message{
-            ID:      i,
-            Content: fmt.Sprintf("Номер сообщения %d", i),
-        }
+	// Основной бесконечный цикл записи сообщений
+	for i := 1; ; i++ {
+		// Создаём сообщение с уникальным ID и текстом
+		msg := &models.Message{
+			ID:      i,
+			Content: fmt.Sprintf("Номер сообщения %d", i),
+		}
 
-        // Сериализуем сообщение в JSON
-        data, _ := msg.Serialize()
-        fmt.Println("Отправляем:", string(data))
+		// Сериализуем сообщение в JSON
+		data, _ := msg.Serialize()
+		fmt.Println("Отправляем:", string(data))
 
-        // Отправляем сообщение в Kafka
-        err := writer.WriteMessages(ctx, kafka.Message{
-            Value: data,
-        })
-        if err != nil {
-            log.Println("Ошибка отправки сообщения:", err)
-        }
+		// Отправляем сообщение в Kafka
+		err := writer.WriteMessages(ctx, kafka.Message{
+			Value: data,
+		})
+		if err != nil {
+			log.Println("Ошибка отправки сообщения:", err)
+		}
 
-        // Ждём 500 мс перед отправкой следующего сообщения
-        time.Sleep(500 * time.Millisecond)
-    }
+		// Ждём 500 мс перед отправкой следующего сообщения
+		time.Sleep(500 * time.Millisecond)
+	}
 }
